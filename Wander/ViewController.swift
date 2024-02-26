2ed4165fc14dac3b7d4bbb4b9efc5c2e23241a91
Alex Cabrera <alcamiz@icloud.com>
2024-02-26 17:08:35 -0600

    Initial Project Files

---
+++ Wander/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wander
+//
+//  Created by Alex Cabrera on 2/26/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
